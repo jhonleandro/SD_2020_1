647a30c098884250d5c4f5baf266b1caecba56ea
Jhônata Leandro <jhonataleandro09@gmail.com>
2021-01-20 14:17:47 -0300

    Add files via upload

---
+++ Tarefa_3/servidor/server.py
@@ -0,0 +1,38 @@
+import rpyc
+import socket
+from const import * #-
+from rpyc.utils.server import ThreadedServer
+ 
+class Legivel(rpyc.Service):
+    value = []
+
+    def exposed_append(self, data):
+        print(f"Concatenando valor: {data}")
+        self.value = self.value + [data]
+        return self.value
+
+    def exposed_value(self):
+        print(f"Retornando valor...")
+        return self.value
+    
+    def exposed_remove(self, data):
+        if data in self.value:
+            print(f"Removendo valor...")
+            self.value.remove(data)
+            return self.value
+
+    def exposed_search(self, data):
+        if data in self.value:
+            print(f"Retornando valor e posição...")
+            return (data, self.value.index(data) + 1)
+
+if __name__ == "__main__":
+  print(f"Criando Servidor Legivel...") 
+  server = ThreadedServer(Legivel, port = PORT)
+  print(f"Conectando ao servidor de diretório...")  
+  conn_ServerDirectory = rpyc.connect(SERVER_DIR,PORT_DIR)
+  print(f"Obtendo endereço IP de Legivel...")  
+  ipAdress = socket.gethostbyname(socket.gethostname())
+  print(f"Registrando no servidor de diretório...") 
+  conn_ServerDirectory.root.exposed_registraServer('Legivel',ipAdress,PORT)
+  server.start()
---
+++ Tarefa_3/servidor_diretorio/server_dir.py
@@ -0,0 +1,62 @@
+from const import *
+import rpyc
+from rpyc.utils.server import ThreadedServer
+
+class ServerDirectory(rpyc.Service):
+    
+    servidorInexistente = f"Servidor não encontrado..."
+    servidorNaoRegistrado = f"Servidor não registrado..."
+    
+    listaDiretorio = {}
+    
+    def __init__(self, lista):
+        self.listaDiretorio = lista
+
+    def exposed_registraServer(self, serverName, ipAdress, portNum):
+        self.listaDiretorio.update({serverName : (ipAdress, portNum)})
+        print(f"Registrando Servidor...")
+        print(self.listaDiretorio)
+
+    def exposed_buscaServer(self, serverName):
+        print(f"Buscando Servidor...")
+
+        if  serverName in self.listaDiretorio:
+            print(f"Servidor encontrado...")
+            print(self.listaDiretorio)
+            return self.listaDiretorio[serverName]
+        else:
+            print(f"Servidor não encontrado...")
+            print(self.listaDiretorio)
+            return self.servidorInexistente
+    
+    def exposed_registraServerNovamente(self, serverName, ipAdress, portNum):
+        print(f"Registrando Servidor Novamente...")
+        
+        if serverName in self.listaDiretorio:
+            print(f"Achou item que vai ser registrado novamente...")
+            self.listaDiretorio[serverName] = (ipAdress, portNum)
+            return self.listaDiretorio[serverName]
+        else:
+            print(f"Item não registrado...")
+            return self.servidorNaoRegistrado
+
+    def exposed_removaServer(self, serverName):
+        print(f"Removendo Servidor...")
+        
+        if  serverName in self.listaDiretorio:
+            print(f"Servidor a ser removido foi encontrado...")
+            elementoRemovido = self.listaDiretorio[serverName]
+            print(f"Guardando o elemento: {elementoRemovido} para ser retornado...")
+            self.listaDiretorio.pop(key=serverName)
+            print(f"Removendo elemento do servidor...")
+            return elementoRemovido
+        else:
+            print(f"Servidor a ser removido não foi encontrado...")
+            return self.serverNaoRegistrado
+
+
+if __name__ == "__main__":
+    listaDiretorio = {}
+    print(f"Iniciando servidor de diretórios na porta: {PORT_DIR}")
+    server_dir = ThreadedServer(ServerDirectory(listaDiretorio), port=12310)
+    server_dir.start()
